4306b36def7a8f6d491162805a639edb993bd992
octalempyrean <octalempyrean@gmail.com>
2019-10-26 20:34:28 +0800

    dalagete Example for product factory

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DelegateExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WrapFactory wrapFactory = new WrapFactory();
+            ProductFactory productFactory = new ProductFactory();
+
+            Func<Product> func1 = new Func<Product>(productFactory.MakePizza);
+            Func<Product> func2 = new Func<Product>(productFactory.MakeToyCar);
+
+            Box box1 = wrapFactory.wrapFactory(func1);
+            Box box2 = wrapFactory.wrapFactory(func2);
+
+            Console.WriteLine(box1.Product.Name);
+            Console.WriteLine(box2.Product.Name);
+
+        }
+    }
+}
+
+class Product
+{
+    public string Name { get; set; }
+}
+
+class Box
+{
+    public Product Product { get; set; }
+}
+
+class WrapFactory
+{
+    public Box wrapFactory(Func<Product> getProduct)
+    {
+        Box box = new Box();
+        Product product = getProduct.Invoke();
+        box.Product = product;
+        return box;
+    }
+}
+
+class ProductFactory
+{
+    public Product MakePizza()
+    {
+        Product product = new Product();
+        product.Name = "Pizza";
+        return product;
+    }
+
+    public Product MakeToyCar()
+    {
+        Product product = new Product();
+        product.Name = "ToyCar";
+        return product;
+    }
+}
